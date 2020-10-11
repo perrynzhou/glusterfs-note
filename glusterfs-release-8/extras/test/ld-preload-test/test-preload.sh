e561d1732717c2beaff5220327d0a1daa3a23a15
root <root@CentOS71.localdomain>
2020-10-11 15:39:23 +0800

    remove old and using git submodule

--- glusterfs-release-8/extras/test/ld-preload-test/test-preload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Testing 32-bit offsets build"
-LD_PRELOAD="$(pwd)/ld-preload-lib.so" $(pwd)/ld-preload-test32 --path /tmp/testfile
-echo
-echo "Testing 64-bit offsets build"
-LD_PRELOAD="$(pwd)/ld-preload-lib.so" $(pwd)/ld-preload-test64 --path /tmp/testfile
--- glusterfs-release-8/tests/basic/glusterfsd-args.t
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/../include.rc
-
-EXPECT $GLUSTER_LIBEXECDIR glusterfsd --print-libexecdir
